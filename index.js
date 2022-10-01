46ed250dbb88efaafe065a2055c775c924f62848
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-10-01 10:03:50 -0300

    [M1S05] - Ex 7 - Instanciando classes

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ import {saudacao} from './saudacao.js';
 
 import {Pedido, Produto} from './database.js';
 
-//console.log(saudacao());
+const prod1 = new Produto("Fusca preto", 40_000, true, 1);
+const prod2 = new Produto("Kombi ciano", 10_000, false, 0);
+const prod3 = new Produto("Chevette verde", 6_500, true, 2);
+const prod4 = new Produto("Opala grafite", 20_000, true, 1);
+const prod5 = new Produto("Maverick vermelho", 35_000, true, 5);
 
-// let novoPedido = new Pedido(1, "Anderson");
-// novoPedido.adicionarProduto = new Produto("calça", 120, true, 25);
+const pedidoAnderson = new Pedido(2022001, "Anderson");
+const pedidoFulano = new Pedido(20220002, "Fulano");
 
-// console.log(novoPedido);
+pedidoAnderson.adicionarProduto(prod1);
+pedidoAnderson.adicionarProduto(prod2);
+pedidoAnderson.adicionarProduto(prod3);
+pedidoFulano.adicionarProduto(prod4);
+pedidoFulano.adicionarProduto(prod5);
